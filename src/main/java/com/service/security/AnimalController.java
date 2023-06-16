f2392a0c1e1730956847bd77f02fc06d1a44491f
Bitsapascal <bitapascal033@gmail.com>
2023-06-16 02:42:40 +0100

    on ajoute les verbes http get,post,put,delete

--- src/main/java/com/service/security/AnimalController.java
+++ src/main/java/com/service/security/AnimalController.java
@@ -5,23 +5,26 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/animals/")
 public class AnimalController {
 	
 	@Autowired
 	private AnimalR an;
 
-	@GetMapping("animals")
+	@GetMapping("get/all")
 	List<Animal> animals() {
 		return an.findAll();
 	}
 	
-	@GetMapping("animals/{id}")
+	@GetMapping("get/one/{id}")
 	Optional<Animal> animal(Double id) {
 		Optional<Animal> animal= an.findById(id);
 		return animal;
@@ -31,5 +34,12 @@ public class AnimalController {
 	Animal add(@RequestParam String nom, @RequestParam String sexe, @RequestParam int qte){
 		Animal a= new Animal(nom,sexe,qte);
 		return an.save(a);
+	}
+	
+	@DeleteMapping("delete/one/{id}")
+	public String delete(Animal id) {
+		an.delete(id);
+		return "Animal was removed successfully ...";
+		
 	}
 }
